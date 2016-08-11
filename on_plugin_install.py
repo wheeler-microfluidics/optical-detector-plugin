ba29a19ba2455edd90347aae634a5ce7459f3f1a
Christian Fobel <christian@fobel.net>
2016-08-11 15:44:47 -0400

    [NB] Update hook scripts (1.1.post12)
    Update hook scripts using [`microdrop-plugin-template==1.1.post12`][1].
    
    [1]: https://github.com/wheeler-microfluidics/microdrop-plugin-template/

--- on_plugin_install.py
+++ on_plugin_install.py
@@ -1,13 +1,26 @@
 from datetime import datetime
 import logging
+import sys
 
 from path_helpers import path
 from pip_helpers import install
 
 
-if __name__ == '__main__':
+def main(ostream=sys.stdout):
     logging.basicConfig(level=logging.INFO)
-    logging.info(str(datetime.now()))
-    requirements_file = path(__file__).parent.joinpath('requirements.txt')
+    plugin_root = path(__file__).parent.abspath()
+    logging.info('[%s] Processing post-install hook for: %s',
+                 str(datetime.now()), plugin_root.name)
+    requirements_file = plugin_root.joinpath('requirements.txt')
     if requirements_file.exists():
-        logging.info(install(['-r', requirements_file]))
+        # Install required packages using `pip`, with Wheeler Lab wheels server
+        # for binary wheels not available on `PyPi`.
+        print >> ostream, install(['--find-links', 'http://192.99.4.95/wheels',
+                                   '--trusted-host', '192.99.4.95', '-r',
+                                   requirements_file])
+        print >> ostream, ('[%s] Completed post-install processing for: %s' %
+                           (str(datetime.now()), plugin_root.name))
+
+
+if __name__ == '__main__':
+    main()
